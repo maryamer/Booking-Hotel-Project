cf6e47c21263750f884d1d6310f6467de2351aa8
maryameb <personalmaryamebrahimi@gmail.com>
2023-09-27 18:42:37 +0330

    useFetch costum hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+function useFetch(url, query = "") {
+  const [data, setData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}?${query}`);
+        setData(data);
+      } catch (err) {
+        setData([]);
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [url, query]);
+  return { isLoading, data };
+}
+
+export default useFetch;
